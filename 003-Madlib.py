d0fa8b1e567385c6c863938fbf24b428e4b1fe3d
Owais Tahir <oo_wais@hotmail.com>
2023-01-28 20:26:48 +0500

    Add files via upload

---
+++ 003-Madlib.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+famous_person = input("Famous Person: ")
+madlib = f"Computer programming is so {adj}! It makes me so {verb1} all the time \
+because I love to {verb2}. Stay hydrated and {verb3} like you are {famous_person}!"
+
+print(madlib)
